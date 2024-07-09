7cfc47a8f13a8c3f226582769ceb4fb0dc22b785
zyyang <zyyang@taosdata.com>
2024-07-09 20:52:19 +0800

    ref: no recall

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use std::fmt::format;
 use std::io::{Read, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
 use std::thread;
 
 fn main() -> anyhow::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8899")
+    const PORT: u16 = 8899;
+
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", PORT))
         .map_err(|err| anyhow::anyhow!("failed to bind to address: {}", err))?;
+    println!("Server started at 127.0.0.1:{}", PORT);
 
     for stream in listener.incoming() {
         let stream = stream.map_err(|err| anyhow::anyhow!("failed to get stream: {}", err))?;
@@ -26,18 +30,23 @@ fn handle_connection(mut stream: TcpStream) {
     const OK: u8 = 0b1111_1111u8;
     const FAIL: u8 = 0b0000_0000u8;
 
-    while match stream.read(&mut buffer) {
-        Ok(size) => {
-            let content = String::from_utf8_lossy(&buffer[..size]);
-            println!("received size: {},content: {}", size, content);
-            stream.write(&[OK]).unwrap();
-            true
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(size) => {
+                if size == 0 {
+                    println!("connection closed");
+                    std::process::exit(0);
+                }
+
+                let content = String::from_utf8_lossy(&buffer[..size]);
+                println!("received size: {},content: {}", size, content);
+                // stream.write(&[OK]).unwrap();
+            }
+            Err(err) => {
+                println!("failed to read data, cause: {:#?}", err);
+                // stream.write(&[FAIL]).unwrap();
+                stream.shutdown(Shutdown::Both).unwrap();
+            }
         }
-        Err(err) => {
-            println!("failed to read data, cause: {:#?}", err);
-            stream.write(&[FAIL]).unwrap();
-            stream.shutdown(Shutdown::Both).unwrap();
-            false
-        }
-    } {}
+    }
 }
